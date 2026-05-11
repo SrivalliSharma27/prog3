9a5771f0c348a1506acf80c54c014372eecdde4f
SrivalliSharma27 <vvce23cseaiml0013@vvce.ac.in>
2026-05-11 23:35:48 +0530

    Update myfile3.java

--- myfile3.java
+++ myfile3.java
@@ -1,5 +1,34 @@
 public class myfile3 {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        pipeline {
+    agent any
+
+    environment {
+        registry = '4vv23ci104/test4'
+        registryCredential = 'jenkin_docker_token'
+        dockerimage = ''
+    }
+
+    stages {
+
+        stage('Checkout') {
+            steps {
+                checkout scmGit(
+                    branches: [[name: '*/main']],
+                    extensions: [],
+                    userRemoteConfigs: [[url: 'https://github.com/SrivalliSharma27/dockerprog.git']]
+                )
+            }
+        }
+
+        stage('Build Docker Image') {
+            steps {
+                script {
+                    dockerimage = docker.build(registry)
+                }
+            }
+        }
+    }
     }
 }
